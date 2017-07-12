c799aa93366cca151ab7c66207f84a5990577189
Gonzalo de Dios <ghdedios@hotmail.com>
2017-07-12 20:31:24 -0300

    commit inicial

---
+++ src/main/java/Glorantha/App.java
@@ -0,0 +1,13 @@
+package Glorantha;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
